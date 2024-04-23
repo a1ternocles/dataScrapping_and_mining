0bbd501e5056b925227311b970bd590787f9626a
Andres Ruiz <anmruizto@gmail.com>
2024-04-22 21:49:55 -0500

    update

--- 1bs4_req.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-r = requests.get('https://quotes.toscrape.com/page/2/')
-html = r.text
-soup = BeautifulSoup(html,'html.parser')
-
-print(type(html))
-print(type(soup))
---
+++ req/1bs4_req.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+r = requests.get('https://quotes.toscrape.com/page/2/')
+html = r.text
+soup = BeautifulSoup(html,'html.parser')
+
+# # print(type(html))
+# print(soup.title.parent)
+
+with open('req\\bs4quotes.txt','w') as file:
+    for tag in soup.findAll('span',{'class': 'text'}):
+        file.write(tag.string)
+        file.write('\n')
