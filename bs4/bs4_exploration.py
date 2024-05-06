288c0243749760ab0ed71b11a5e53b23c774c17f
Andres Ruiz <anmruizto@gmail.com>
2024-05-06 00:14:59 -0500

    update

---
+++ bs4/bs4_exploration.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+
+html ='<b id = "xyz" class = "abc"> Hello World </b></span><span></span>'
+soup = BeautifulSoup(html, 'html.parser')
+tag = soup.b
+
+# print(tag['id'])
+print(tag['class'])
